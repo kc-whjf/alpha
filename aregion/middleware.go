4ea7c29e569e06872b18e2230f67e8c12fdd3825
qianmingjun <qianmingjun@kingsoft.com>
2024-06-14 09:02:12 +0800

    code optimize

--- aregion/middleware.go
+++ aregion/middleware.go
@@ -1,26 +1,17 @@
 package aregion
 
 import (
-	"fmt"
 	"github.com/go-resty/resty/v2"
 	"github.com/kc-whjf/alpha/aconfig"
-	"github.com/kc-whjf/alpha/alog"
 	"github.com/kc-whjf/alpha/httpclient"
-	"strings"
 )
 
 func NewRestyWith(portName aconfig.PortName, appConfig *aconfig.Application, protocol string) (*resty.Client, error) {
-	location := appConfig.GetMatchedPrimaryPortLocation(portName)
-	if location == nil {
-		return nil, fmt.Errorf("missing matched primaryport location (port_name: %s)", portName)
+	client, err := httpclient.NewRestyWith(portName, appConfig, protocol)
+	if err != nil {
+		return nil, err
 	}
 
-	client := httpclient.NewResty(alog.Sugar)
-	// Eliminat the interference of protocol in address
-	address := strings.TrimPrefix(location.Address, "http://")
-	address = strings.TrimPrefix(address, "https://")
-	hostURL := fmt.Sprintf("%s%s:%d", protocol, address, location.Port)
-	client.SetHostURL(hostURL)
 	AddCrossRegionMiddleware(client)
 
 	return client, nil
@@ -46,7 +37,7 @@ func crossRegionMiddleware(c *resty.Client, req *resty.Request) (err error) {
 	}
 
 	req.Header.Add("X-Ops-Raw-Url", req.URL)
-	//rewrite to ops-console
+	//rewrite to cross region server
 	req.URL = crossRegionUrl
 	return nil
 }
